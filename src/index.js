5d213513259fa7239a36cc465ef13afcadc14301
Kevin Yuvi <kevinyuviuce@gmail.com>
2025-05-13 18:29:59 -0500

    quitamos encabezado

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import "./styles.css";
 
 const App = () => (
   <Router>
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
-    </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
