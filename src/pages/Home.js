d68778fce57de80863bea57b467c44c7aa6822b8
Kevin Yuvi <kevinyuviuce@gmail.com>
2025-05-13 18:31:11 -0500

    cambio inicio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Inicio</h1>
       <p>Contador: {count}</p>
       <button onClick={() => setCount(count + 1)}>Incrementar</button>
     </div>
